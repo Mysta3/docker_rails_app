9d5971d92d0d05c31f087b20e0ba5116590d2cc8
Mysta3 <myshawnestallings@gmail.com>
2023-02-24 09:21:02 -0800

    fin

--- app/models/wiki_post.rb
+++ app/models/wiki_post.rb
@@ -1,3 +1,6 @@
 class WikiPost < ApplicationRecord
   has_one_attached :image
+  def meta
+    "Created By: #{self.author} on #{self.created_at.to_formatted_s(:short)} and last updated #{self.updated_at.to_formatted_s(:short)}"
+  end
 end
